6842e53e8d868e81ea38920c56c54612cfe50e39
JangKroed <didlsdydgh@gmail.com>
2024-04-26 15:47:34 +0900

    add to posts [GET]

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,65 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjwans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjwans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 공연중인 로제',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+];
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts(): PostModel[] {
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: string): PostModel {
+    const post = posts.find((post) => post.id === Number(id));
+
+    if (!post) throw new NotFoundException();
+
+    return post;
+  }
 }
